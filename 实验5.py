9d4ae05f815be8c9c991c89211e4bbded0755b89
唯爱羽嫣 <anlin781205936@126.com>
2016-11-09 01:43:54 +0800

    Add files via upload

--- 实验5.py
+++ 实验5.py
@@ -1,14 +1,22 @@
 #!usr/bin/python
 # -*-coding:utf-8 -*-
 #序列求和
+import math
 
 def main():
 	a = int(input("请输入a:"))
 	n = int(input("请输入n:"))
 	l = []
 	
+	temp = 0
+	for i in range(1,n+1):
+		l.append(temp + a*math.pow(10,i-1))
+		temp = l[i-1]
+	
+	temp = 0	
 	for i in range(0,n):
-			
+		temp += l[i]
+	print(int(temp))
 
 if __name__ == "__main__":
 	main()
